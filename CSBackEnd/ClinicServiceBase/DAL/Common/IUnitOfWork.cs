ba7873819492d76b2cafc2bf84bd31b6d34635ae
Alexey Ivanov <Nothing.Is.Real@yandex.ru>
2026-07-02 17:04:06 +0300

    add uow repository

---
+++ CSBackEnd/ClinicServiceBase/DAL/Common/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using ClinicServiceBase.DAL.DBRepositories;
+
+namespace ClinicServiceBase.DAL.Common
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void CommitToDB();
+
+        Task CommitToDBAsync(CancellationToken cancellationToken);
+
+        void CommitWithUpdateTime(bool updateTime);
+
+        Task CommitWithUpdateTimeAsync(bool updateTime, CancellationToken cancellationToken);
+
+        void Register(IClinicServiseRepository repository);
+
+        T GetRepository<T>() where T : IClinicServiseRepository;
+    }
+}
---
+++ CSBackEnd/ClinicServiceBase/DAL/DBRepositories/IClinicServiseRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+namespace ClinicServiceBase.DAL.DBRepositories
+{
+    public interface IClinicServiseRepository
+    {
+    }
+
+    public interface IClinicServiceRepository<TObj> : IClinicServiseRepository where TObj : class
+    {
+
+        public Task<TObj[]> GetAllObjects(CancellationToken cancellationToken);
+        
+        public Task<TObj[]> GetObjectsByIds(Guid[] ids, CancellationToken cancellationToken);
+
+        public Task<TObj> GetObjectsById(Guid id, CancellationToken cancellationToken);
+
+        public Task<TObj> GetObjectsByFilter(Expression<Func<TObj, bool>> filter, CancellationToken cancellationToken);
+
+        public Task AddObject(TObj obj);
+
+        public Task UpdateObject(TObj obj, TObj dto = null);
+
+        public Task DeleteObject(TObj obj);
+
+        public Task DeleteObjectById(Guid id);
+
+        public Task SoftDeleteById(Guid Id);
+
+        public Task SoftDeleteByFilter(Expression<Func<TObj, bool>> filter);
+    }
+}
