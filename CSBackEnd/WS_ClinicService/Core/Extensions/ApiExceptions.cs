20faab6fc361df91b255c0ee72238e14e2f408ad
Mast3rm1nd <Nothing.Is.Real@yandex.ru>
2026-08-12 21:30:11 +0300

    1

--- CSBackEnd/WS_ClinicService/Core/Extensions/ApiExceptions.cs
+++ CSBackEnd/WS_ClinicService/Core/Extensions/ApiExceptions.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ClinicServiceBase.Common.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace WS_ClinicService.Core.Extensions
 {
@@ -7,8 +10,18 @@ namespace WS_ClinicService.Core.Extensions
         public void OnException(ExceptionContext context)
         {
             switch (context.Exception)
-            { 
-                
+            {
+                case BadRequestException badRequest:
+                    var result = new ObjectResult(new ProblemDetails
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Title = badRequest.Message,
+                        Detail = badRequest.Message,
+                        Instance = context.HttpContext.Request.Path
+                    });
+
+                    context.Result = result;
+                    break;
             }
         }
     }
--- CSBackEnd/WS_ClinicService/Program.cs
+++ CSBackEnd/WS_ClinicService/Program.cs
@@ -1,4 +1,12 @@
 using Autofac.Extensions.DependencyInjection;
+using WS_ClinicService.Core.Extensions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +15,20 @@ var services = builder.Services;
 services.AddControllers(conf =>
 {
     conf.EnableEndpointRouting = false;
-    
+    conf.Filters.Add(typeof(ApiExceptions));
+}).AddNewtonsoftJson(x =>
+{
+    x.SerializerSettings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+    x.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+}).ConfigureApiBehaviorOptions(y =>
+{
+    y.SuppressModelStateInvalidFilter = false;
+    y.InvalidModelStateResponseFactory = c =>
+    {
+        return new UnprocessableEntityObjectResult(c.ModelState);
+    };
 });
+
 
 builder.Services.AddOpenApi();
 
